d58d0fdd93524cca235dd2893aa3e70d4e4da94e
wesslingpedro <wesslingpedro@gmail.com>
2024-05-15 19:59:32 -0300

    Primeiro commit

---
+++ ECOMPOSTA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace testes_ecc{
+    class Program{
+        static void Main(string[] args){
+
+            int horas;
+
+            Console.WriteLine("Quantas horas? ");
+            horas = int.Parse(Console.ReadLine());
+
+
+            if (horas < 12){
+                Console.WriteLine("Bom dia!");
+            }
+            else{
+                Console.WriteLine("Boa tarde!");
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
